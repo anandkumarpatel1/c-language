ecb784343f89887904c48bb3cb6f613dcd1e2c51
anand kumar <harshrajsinha1@gmail.com>
2023-01-08 23:24:46 +0530

    c practice

--- 2d_table_.c
+++ 2d_table_.c
@@ -1,27 +1,31 @@
 #include <stdio.h>
 int main()
 {
-    int arr[3][10];
+    int e ,f;
+    printf("Enter the length of the table \n");
+    scanf("%d",&e);
+    f = e;
+    int arr[3][e];
     int a, b ,c;
     printf("Enter the table which you want \n");
     scanf("%d %d %d",&a , &b , &c);
     for (int i = 0; i < 1;i++)
     {
-        for (int j = 0; j < 10; j++)
+        for (int j = 0; j < f; j++)
         {
             arr[i][j] = a*(j+1);
         } 
     }
     for (int i = 1; i < 2;i++)
     {
-        for (int j = 0; j < 10; j++)
+        for (int j = 0; j < f; j++)
         {
             arr[i][j] = b*(j+1);
         } 
     }
     for (int i = 2; i < 3;i++)
     {
-        for (int j = 0; j < 10; j++)
+        for (int j = 0; j < f; j++)
         {
             arr[i][j] = c*(j+1);
         } 
@@ -31,28 +35,31 @@ int main()
 
     for (int i = 0; i < 1;i++)
     {
-        for (int j = 0; j < 10; j++)
+        for (int j = 0; j < f; j++)
         {
             printf("%d x %d = %d\n",a,j+1,arr[i][j]);
         }
         
     }
+    printf("-------******* the table is end *******------- \n \n");
     for (int i = 1; i < 2;i++)
     {
-        for (int j = 0; j < 10; j++)
+        for (int j = 0; j < f; j++)
         {
             printf("%d x %d = %d\n",b,j+1,arr[i][j]);
         }
         
     }
+    printf("-------******* the table is end *******------- \n");
     for (int i = 2; i < 3;i++)
     {
-        for (int j = 0; j < 10; j++)
+        for (int j = 0; j < f; j++)
         {
             printf("%d x %d = %d\n",c,j+1,arr[i][j]);
         }
         
     }
+    printf("-------******* the table is end *******------- \n");
 
 
 return 0;
